fdb360a47e019930a7940006fe9fe91dccc07c50
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-15 17:32:40 +0200

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,43 +1,45 @@
-import { useEffect } from "react"
-import {Container, Row, Col} from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-const ENDPOINT = "https://api.spaceflightnewsapi.net/v4/articles/:id"
+const ENDPOINT = "https://api.spaceflightnewsapi.net/v4/articles/:id";
 
-const Home = function (){
+const Home = function () {
 
-    const getNews = async () => {
-        try {
-            const response = await fetch(ENDPOINT)
-            if(response.ok){
-                const data = await response.json()
-            } else {
-                throw new Error("Errore nel recupero dei dati")
-            }
-        } catch (error) {
-            console.log("Errore", error)
-        }
+    const [apiResponse, setApiResponse] = useState<APIResponse | null>(null);
+
+
+  const getNews = async () => {
+    try {
+      const response = await fetch(ENDPOINT);
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Data", data);
+      } else {
+        throw new Error("Errore nel recupero dei dati");
+      }
+    } catch (error) {
+      console.log("Errore", error);
     }
+  };
 
-    useEffect(() => {
-     getNews()
-    })
-    
+  useEffect(() => {
+    getNews();
+  }, []);
 
-    return (
-        <>
-        <Container>
-            <Row>
-                <Col xs={12} md={6} className="justify-content-center">
-                <h1>Space News Blog</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={12} md={6} className="justify-content-center">
-                </Col>
-            </Row>
-        </Container>
-        </>
-    )
-}
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col xs={12} md={6} className='justify-content-center'>
+            <h1>Space News Blog</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={6} className='justify-content-center'></Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
-export default Home
+export default Home;
