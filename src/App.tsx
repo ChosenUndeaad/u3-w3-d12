885b56f1c47d8b4ac785ae158cb73bbcc45acf06
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-04-15 17:28:46 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import MyNavbar from './components/Navbar';
+import Home from './components/Home.tsx'
 
 function App() {
   return (
     <>
+    <MyNavbar />
+    <Home />
     </>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react"
+import {Container, Row, Col} from "react-bootstrap"
+
+const ENDPOINT = "https://api.spaceflightnewsapi.net/v4/articles/:id"
+
+const Home = function (){
+
+    const getNews = async () => {
+        try {
+            const response = await fetch(ENDPOINT)
+            if(response.ok){
+                const data = await response.json()
+            } else {
+                throw new Error("Errore nel recupero dei dati")
+            }
+        } catch (error) {
+            console.log("Errore", error)
+        }
+    }
+
+    useEffect(() => {
+     getNews()
+    })
+    
+
+    return (
+        <>
+        <Container>
+            <Row>
+                <Col xs={12} md={6} className="justify-content-center">
+                <h1>Space News Blog</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} md={6} className="justify-content-center">
+                </Col>
+            </Row>
+        </Container>
+        </>
+    )
+}
+
+export default Home
